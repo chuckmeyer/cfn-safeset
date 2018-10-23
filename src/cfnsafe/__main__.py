686b51f8ddb0bca87be6c43ebe0ad3d857642f44
Chuck Meyer <cmmeyer@amazon.com>
2018-10-23 15:09:50 -0400

    Allow cfn-safe to read change set from file (-f)

--- src/cfnsafe/__main__.py
+++ src/cfnsafe/__main__.py
@@ -24,9 +24,7 @@ CONFIG_FILE = '/data/stateful-resources.yaml'
 
 def main():
     """Main function"""
-    stateful_replace = False
     args = cfnsafe.core.get_args()
-    cfnsafe.core.init_logger(args.debug)
     config = cfnsafe.core.init_config(CONFIG_FILE)
     monitored_change_types = config['ChangeTypes']
     LOGGER.debug('Monitored change types from config: %s',
@@ -36,29 +34,12 @@ def main():
     if args.list:
         cfnsafe.core.show_stateful_resources(stateful_resources)
         return 0
-    changes = cfnsafe.core.get_change_set(
-        args.changeset, args.stack, args.region)
-    for change in changes:
-        if change['Type'] in monitored_change_types:
-            LOGGER.debug('Monitored resource type: %s', change['Type'])
-            if cfnsafe.core.is_stateful_replace(
-                    change[monitored_change_types[change['Type']]],
-                    stateful_resources
-            ):
-                properties = cfnsafe.core.stateful_replace_properties(
-                    change[monitored_change_types[change['Type']]])
-                LOGGER.warning(
-                    'Replace required for stateful resource %s (%s)'
-                    'due to changes to these properties: %s',
-                    change['ResourceChange']['LogicalResourceId'],
-                    change['ResourceChange']['ResourceType'],
-                    list(properties))
-                stateful_replace = True
-            else:
-                LOGGER.info('Non-stateful resource skipped: %s (%s)',
-                            change['ResourceChange']['LogicalResourceId'],
-                            change['ResourceChange']['ResourceType'])
-    if stateful_replace:
+    if args.file:
+        changes = cfnsafe.core.load_cs_file(args.file)
+    else:
+        changes = cfnsafe.core.get_change_set(
+            args.changeset, args.stack, args.region)
+    if cfnsafe.core.detect_stateful_replace(changes, monitored_change_types, stateful_resources):
         return 2
     return 0
 
--- src/cfnsafe/core.py
+++ src/cfnsafe/core.py
@@ -19,10 +19,12 @@ import logging
 import argparse
 import sys
 import os
+import json
 import pkg_resources
 from boto3 import client
 from botocore.exceptions import ClientError
 import yaml
+from cfnsafe.version import __version__
 
 LOGGER = logging.getLogger('cfnsafe')
 
@@ -30,13 +32,20 @@ LOGGER = logging.getLogger('cfnsafe')
 def init_logger(use_debug):
     """ Set log level and format """
     handler = logging.StreamHandler()
-    formatter = logging.Formatter(
-        #    '%(asctime)s %(levelname)-8s %(message)s')
-        '%(levelname)-8s %(message)s')
-    handler.setFormatter(formatter)
-    LOGGER.addHandler(handler)
+    handler.setLevel(logging.DEBUG)
+
     if use_debug:
         LOGGER.setLevel(logging.DEBUG)
+    else:
+        LOGGER.setLevel(logging.WARNING)
+
+    formatter = logging.Formatter('%(levelname)-8s %(message)s')
+        #    '%(asctime)s %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    # make sure all other log handlers are removed before adding it back
+    for handler in LOGGER.handlers:
+        LOGGER.removeHandler(handler)
+    LOGGER.addHandler(handler)
 
 
 def init_config(config_file):
@@ -53,9 +62,9 @@ def init_config(config_file):
 
 
 def create_parser():
-    """ Set up command line arguements """
+    """ Set up command line arguments """
     parser = argparse.ArgumentParser(
-        description='CloudFormation ChangSet Arbiter')
+        description='CloudFormation ChangeSet safety check')
 
     standard = parser.add_argument_group('Standard')
     advanced = parser.add_argument_group('Advanced / Debugging')
@@ -67,13 +76,18 @@ def create_parser():
         '-s', '--stack', metavar='STACKNAME',
         help='The stack name associated with this change set')
     standard.add_argument(
+        '-f', '--file', metavar='STACKNAME',
+        help='File containing a valid CloudFormation change set')
+    standard.add_argument(
         '-r', '--region', metavar='REGION', default='us-east-1',
         help='The region where this change set exists')
+    standard.add_argument(
+        '-v', '--version', help='Version of cfn-safe', action='version',
+        version='%(prog)s {version}'.format(version=__version__))
     advanced.add_argument(
         '-d', '--debug', help='Enable debug logging', action='store_true')
     advanced.add_argument(
-        '-l', '--list', help='List stateful resources', action='store_true'
-    )
+        '-l', '--list', help='List stateful resources', action='store_true')
     return parser
 
 
@@ -83,19 +97,19 @@ def get_args():
     args = parser.parse_args()
     if args.list:
         return args
-    if not args.changeset:
-        LOGGER.error('%s: You must specify a valid change set and stack name (-c/-s)',
-                     os.path.basename(sys.argv[0]))
-        sys.exit(1)
-    if not args.stack:
-        LOGGER.error('%s: You must specify a valid change set and stack name (-c/-s)',
+
+    init_logger(args.debug)
+
+    if (not args.changeset and not args.stack) and not args.file:
+        LOGGER.error('%s: You must specify a valid change set and stack name (-c/-s)'
+                     'or file location (-f)',
                      os.path.basename(sys.argv[0]))
         sys.exit(1)
     return args
 
 
 def get_change_set(change_set, stack, region):
-    """ Retrieve ChangeSet data via API """
+    """ Retrieve change set data via API """
     LOGGER.debug('Retrieving change set %s for stack %s in region %s',
                  change_set, stack, region)
     try:
@@ -120,6 +134,28 @@ def get_change_set(change_set, stack, region):
         sys.exit(1)
 
 
+def load_cs_file(filename):
+    """ Retrieve change set data from a file """
+    try:
+        with open(filename) as change_file:
+            change_set = json.load(change_file)
+        return change_set['Changes']
+    except IOError as err:
+        if err.errno == 2:
+            LOGGER.error('Change set file not found: %s', filename)
+        elif err.errno == 21:
+            LOGGER.error(
+                'Change set references a directory, not a file: %s', filename)
+        elif err.errno == 13:
+            LOGGER.error(
+                'Permission denied when accessing change set file: %s', filename)
+        sys.exit(1)
+    except Exception as json_err: # pylint: disable=W0703
+        LOGGER.error(
+            'Tried to parse %s as JSON but got error: %s', filename, str(json_err))
+        sys.exit(1)
+
+
 def show_stateful_resources(stateful_resources):
     """ List stateful resources defined in resource data """
     print('List of stateful resources:')
@@ -127,9 +163,45 @@ def show_stateful_resources(stateful_resources):
         print(' * %s' % resource)
 
 
-def is_stateful_replace(change, stateful_resources):
+def is_stateful(change, stateful_resources):
     """ Boolean check if current change references a stateful resource """
-    return change['Replacement'] == 'True' and change['ResourceType'] in stateful_resources
+    return change['ResourceType'] in stateful_resources
+
+
+def is_replace(change):
+    """ Boolean check if current change references a stateful resource """
+    return change['Replacement'] == 'True'
+
+
+def detect_stateful_replace(changes, monitored_change_types, stateful_resources):
+    """ Iterate through changes and look for stateful resources with replace actions """
+    stateful_replace = False
+    for change in changes:
+        if change['Type'] in monitored_change_types:
+            LOGGER.debug('Monitored resource type: %s', change['Type'])
+            if is_stateful(
+                    change[monitored_change_types[change['Type']]],
+                    stateful_resources
+            ):
+                LOGGER.info('Stateful resource detected: %s (%s)',
+                            change['ResourceChange']['LogicalResourceId'],
+                            change['ResourceChange']['ResourceType'])
+                if is_replace(
+                        change[monitored_change_types[change['Type']]]):
+                    properties = stateful_replace_properties(
+                        change[monitored_change_types[change['Type']]])
+                    LOGGER.warning(
+                        'Replace required for stateful resource %s (%s)'
+                        'due to changes to these properties: %s',
+                        change['ResourceChange']['LogicalResourceId'],
+                        change['ResourceChange']['ResourceType'],
+                        list(properties))
+                    stateful_replace = True
+            else:
+                LOGGER.info('Non-stateful resource skipped: %s (%s)',
+                            change['ResourceChange']['LogicalResourceId'],
+                            change['ResourceChange']['ResourceType'])
+    return stateful_replace
 
 
 def stateful_replace_properties(change):
